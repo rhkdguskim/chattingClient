37239db53bd004daa0628593095c457041ecd15a
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2024-02-25 18:26:47 +0900

    fix : cors policy fixed

--- setupProxy.js
+++ setupProxy.js
@@ -3,23 +3,23 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (app) => {
   app.use(
     createProxyMiddleware('/auth', {
-      target: 'http://localhost:3000/',
+      target: 'http://localhost:8080/',
       changeOrigin: true,
     }),
     createProxyMiddleware('/users', {
-      target: 'http://localhost:3000/',
+      target: 'http://localhost:8080/',
       changeOrigin: true,
     }),
     createProxyMiddleware('/friend', {
-      target: 'http://localhost:3000/',
+      target: 'http://localhost:8080/',
       changeOrigin: true,
     }),
     createProxyMiddleware('/chatting', {
-      target: 'http://localhost:3000/',
+      target: 'http://localhost:8080/',
       changeOrigin: true,
     }),
     createProxyMiddleware('/file', {
-      target: 'http://localhost:3000/',
+      target: 'http://localhost:8080/',
       changeOrigin: true,
     }),
   );
